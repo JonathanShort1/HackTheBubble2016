3efa9b4e6fdd3014101b8b1703dc1c9cf69ce933
Peter <retepmorton@gmail.com>
2016-10-08 12:26:45 +0100

    added getters

--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,27 @@
+/**
+ * 
+ */
+
 public class Ship {
+    private int ammo;
+    private double velocity;
+    private double angle;
+    private Bullet[] bullets;
+
+    public int getAmmo() {
+        return 0;
+    }
+
+    public double getVelocity() {
+        return 0;
+    }
+
+    public double getAngle() {
+        return 0;
+    }
+
+    public Bullet[] getBullets(){
+        return bullets;
+    }
+
 }
