a000cced11bd1c365a942539a18c0a6d32befb8b
Jonathan <jonathan.short@live.co.uk>
2016-10-08 12:45:41 +0100

    added boolean 'inFlight' for bullet with getter and setter

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,2 +1,21 @@
-public class Bullet {
+
+public class Bullet extends Entity {
+
+    private boolean inFlight;
+
+    public Bullet() {
+        super.setxPos(0);
+        super.setyPos(0);
+        super.setVelocity(0);
+        super.setAngle(0);
+        this.inFlight = true;
+    }
+
+    public boolean isInFlight() {
+        return inFlight;
+    }
+
+    public void setInFlight(boolean inFlight) {
+        this.inFlight = inFlight;
+    }
 }
